7560c2d8af728300521a7b848f979c537ae883f4
Mateusz Kołomański <63905560+m-kolomanski@users.noreply.github.com>
2026-05-25 12:32:44 +0000

    fix r-lsp timeout warning

--- lua/plugins/r-lsp.lua
+++ lua/plugins/r-lsp.lua
@@ -5,6 +5,15 @@ return {
       r_language_server = function(server)
         vim.lsp.config(server, {
           cmd = { "R", "--no-echo", "-e", "languageserver::run()" },
+          flags = {
+            allow_incremental_sync = true,
+            debounce_text_changes = 500,
+          },
+          capabilities = {
+            textDocumentSync = {
+              willSaveWaitUntil = false,
+            },
+          },
         })
         vim.lsp.enable(server)
       end,
